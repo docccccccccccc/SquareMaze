61f9555a4336b72dfd506540b81a721dd326b85e
doctypehtml <195564139+docccccccccccc@users.noreply.github.com>
2026-02-21 19:54:22 +0800

    将关卡更新到第 13 关，并且添加了分页功能

--- src/views/SelectLevel.vue
+++ src/views/SelectLevel.vue
@@ -4,8 +4,28 @@ import BackButton from '@/components/BackButton.vue';
 import LevelLink from '@/components/LevelLink.vue';
 import { usePlayerInfoStore } from '@/stores/playerInfo';
 import { storeToRefs } from 'pinia';
+import { ref, computed } from 'vue';
 
 const { playerInfo } = storeToRefs(usePlayerInfoStore())
+
+const currentPage = ref(1)
+
+// 每页显示的关卡数量
+const pageSize = 10
+
+// 关卡范围
+const currentLevelRange = computed(() => {
+  const startLevel = (currentPage.value - 1) * pageSize + 1
+  const endLevel = Math.min(currentPage.value * pageSize, playerInfo.value.highestLevel)
+
+  // 关卡 ID 数组
+  const levels: number[] = []
+  for (let i = startLevel; i <= endLevel; i++) {
+    levels.push(i)
+  }
+
+  return levels
+})
 </script>
 
 <template>
@@ -15,8 +35,10 @@ const { playerInfo } = storeToRefs(usePlayerInfoStore())
       <BackButton />
     </h1>
     <el-space fill direction="vertical" style="width: 100%">
-      <LevelLink :id="levelId" v-for="levelId in playerInfo.highestLevel" :key="levelId" />
+      <LevelLink :id="levelId" v-for="levelId in currentLevelRange" :key="levelId" />
     </el-space>
+    <el-pagination v-model:current-page="currentPage" :total="playerInfo.highestLevel" layout="prev, pager, next"
+      :page-size="pageSize" hide-on-single-page />
   </MiddleLayout>
 </template>
 
